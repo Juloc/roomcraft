befc0faad27a88e77ed46397c9ece9e35c82dc70
Juloc <juli.hiresch@gmail.com>
2026-09-22 22:07:25 +0200

    Repair PostgreSQL data permissions on startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -eu
 
-install -d -o postgres -g postgres "$PGDATA" /data/assets
+install -d -o postgres -g postgres /data/assets
+
+if [ -e "$PGDATA" ]; then
+    chown -R postgres:postgres "$PGDATA"
+    chmod 0700 "$PGDATA"
+else
+    install -d -m 0700 -o postgres -g postgres "$PGDATA"
+fi
 
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
     gosu postgres initdb -D "$PGDATA" --auth-local=trust --auth-host=trust
